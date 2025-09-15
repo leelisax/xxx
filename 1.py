7732031f101570bd603be5974929b68d8879afbb
leelisax <lizagalkina08@gmail.com>
2025-09-15 20:42:15 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+#организация стека
+animals=[‘fox’,  ‘bear’, ‘cat’, ‘dog’]
+#сохраним полученные переменные в список
+stack=
+stack.append(‘fox’)
+stack.append(‘bear’)
+stack.append(‘cat’)
+stack.append(‘dog’)
+
